f1e72133906bd8de80c8596c6e74789b67d72b52
dariogdominguez <dominguez.dario@gmail.com>
2024-12-12 14:40:20 -0300

    Se sube proyecto base

---
+++ src/test/java/com/meli/be_java_hisp_w28_g01/BeJavaHispW28G01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.be_java_hisp_w28_g01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW28G01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
