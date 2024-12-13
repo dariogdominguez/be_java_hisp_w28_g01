2c5e80fb0ff3d4217de1add302dfb33312baee49
palomasosa <palososa_19@hotmail.es>
2024-12-13 15:09:00 -0300

    fixes en SellerService

--- src/main/java/com/meli/be_java_hisp_w28_g01/service/impl/SellerServiceImpl.java
+++ src/main/java/com/meli/be_java_hisp_w28_g01/service/impl/SellerServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SellerServiceImpl implements ISellerService {
@@ -19,12 +20,12 @@ public class SellerServiceImpl implements ISellerService {
     }
 
     @Override
-    public Seller findById(int id){
-        return sellerRepository.getAll()
+    public Optional<Seller> findById(int id){
+        Optional<Seller> seller = sellerRepository.getAll()
                 .stream()
                 .filter(m -> m.getId() == id)
-                .findFirst()
-                .orElse(null);
+                .findFirst();
+        return seller;
     }
 
 }
