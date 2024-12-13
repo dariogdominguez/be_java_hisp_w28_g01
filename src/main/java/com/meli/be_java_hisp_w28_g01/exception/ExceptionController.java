9b6789be6323990093399124a5a5b59c604029cd
Mateo Ferradans <mateo.ferradans@mercadolibre.com>
2024-12-13 17:19:45 -0300

    creación de excepciones

---
+++ src/main/java/com/meli/be_java_hisp_w28_g01/exception/ExceptionController.java
@@ -0,0 +1,36 @@
+package com.meli.be_java_hisp_w28_g01.exception;
+
+import com.meli.be_java_hisp_w28_g01.dto.ExceptionDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ExceptionDto> notFound(NotFoundException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionDto);
+    }
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<ExceptionDto> alreadyExists(AlreadyExistsException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionDto);
+
+    }
+    @ExceptionHandler(FollowAlreadyExistsException.class)
+    public ResponseEntity<ExceptionDto> followAlreadyExists(FollowAlreadyExistsException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(exceptionDto);
+
+    }
+    @ExceptionHandler(IsEmptyException.class)
+    public ResponseEntity<ExceptionDto> isEmpty(IsEmptyException e) {
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDto);
+
+    }
+}
