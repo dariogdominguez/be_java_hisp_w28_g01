754884c9bcda7323b13dc13c1e08f5b9180d4d1f
melasim <melania.simes@hotmail.com>
2024-12-13 15:10:23 -0300

    se efectúan cambios

--- src/main/java/com/meli/be_java_hisp_w28_g01/model/Seller.java
+++ src/main/java/com/meli/be_java_hisp_w28_g01/model/Seller.java
@@ -1,11 +1,5 @@
 package com.meli.be_java_hisp_w28_g01.model;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.FieldDefaults;
-
 
 public class Seller extends User{
 
