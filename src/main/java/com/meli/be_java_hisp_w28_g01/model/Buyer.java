3e6e4fc119f9ba7cdb2b97aec0b9109a42fa053d
dariogdominguez <dominguez.dario@gmail.com>
2024-12-13 14:24:01 -0300

    Se agregan anotaciones a la entidad Buyer se define una version de lombok y se quita la a notacion de NoArgsConstructor de Seller.

--- src/main/java/com/meli/be_java_hisp_w28_g01/model/Buyer.java
+++ src/main/java/com/meli/be_java_hisp_w28_g01/model/Buyer.java
@@ -1,4 +1,12 @@
 package com.meli.be_java_hisp_w28_g01.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Buyer extends User{
 }
