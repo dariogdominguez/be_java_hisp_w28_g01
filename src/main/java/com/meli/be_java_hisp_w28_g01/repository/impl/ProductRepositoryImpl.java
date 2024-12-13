dc3a9b8f86d973f0800e48f7f813cbe1741e4b6c
Facundo Delavalle <80860340+fdelavalle@users.noreply.github.com>
2024-12-13 17:25:27 -0300

    Revert "Refactor load database"

--- src/main/java/com/meli/be_java_hisp_w28_g01/repository/impl/ProductRepositoryImpl.java
+++ src/main/java/com/meli/be_java_hisp_w28_g01/repository/impl/ProductRepositoryImpl.java
@@ -1,19 +1,24 @@
 package com.meli.be_java_hisp_w28_g01.repository.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.meli.be_java_hisp_w28_g01.model.Product;
 import com.meli.be_java_hisp_w28_g01.repository.IProductRepository;
-import com.meli.be_java_hisp_w28_g01.util.DataLoader;
 import org.springframework.stereotype.Repository;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 class ProductRepositoryImpl implements IProductRepository {
-    private final List<Product> listOfProduct;
-    private final String filePath = "product.json";
 
-    public ProductRepositoryImpl(DataLoader dataLoader) {
-        listOfProduct = dataLoader.loadDataBase(filePath);
+    private List<Product> listOfProduct = new ArrayList<>();
+
+    public ProductRepositoryImpl() throws IOException {
+        listOfProduct = loadDataBase();
     }
 
     @Override
@@ -21,5 +26,25 @@ class ProductRepositoryImpl implements IProductRepository {
         return listOfProduct;
     }
 
+    public List<Product> loadDataBase() {
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("product.json")) {
+            if (inputStream == null) {
+                throw new RuntimeException("JSON no encontrado.");
+            }
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            TypeReference<List<Product>> typeRef = new TypeReference<>() {
+            };
+            List<Product> products = null;
+            try {
+                products = objectMapper.readValue(inputStream, typeRef);
+            } catch (IOException e) {
+                throw new RuntimeException("No se pusieron obtener los datos de los productos.");
+            }
+            return products;
+        } catch (IOException e) {
+            throw new RuntimeException("Error al acceder al JSON.", e);
+        }
+    }
 }
 
