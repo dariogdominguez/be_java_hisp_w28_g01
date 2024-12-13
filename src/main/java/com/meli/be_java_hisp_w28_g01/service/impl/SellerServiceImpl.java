61afd21865a14210e3bcd925ec983eaa3cc8e9f8
Mateo Ferradans <mateo.ferradans@mercadolibre.com>
2024-12-13 12:43:20 -0300

    cambio sellerserv

--- src/main/java/com/meli/be_java_hisp_w28_g01/service/impl/SellerServiceImpl.java
+++ src/main/java/com/meli/be_java_hisp_w28_g01/service/impl/SellerServiceImpl.java
@@ -2,10 +2,14 @@ package com.meli.be_java_hisp_w28_g01.service.impl;
 
 import com.meli.be_java_hisp_w28_g01.model.Seller;
 import com.meli.be_java_hisp_w28_g01.service.ISellerService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+
 public class SellerServiceImpl implements ISellerService {
+    SellerRepository sellerRepository;
+
     @Override
     public List<Seller> getAll(){
         return sellerRepository.findAll();
